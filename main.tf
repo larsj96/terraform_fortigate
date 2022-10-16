f51b9b585b86eab10dfe74fdeab125db82c5d06c
Lars Jørgen# <ljn@aztek.no>
2022-10-16 02:31:40 +0200

    test again with provider

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
+
+terraform {
+  required_providers {
+    fortios = {
+      source = "fortinetdev/fortios"
+      version = "1.16.0"
+    }
+  }
+}
+
+
+
 resource "fortios_system_setting_dns" "dns" {
   primary   = "1.1.1.1"
   secondary = "1.1.2.2"
 }
+
 
