3d93794acd188763116f5e5719ef578878d17957
Lars Jørgen# <ljn@aztek.no>
2022-10-23 14:03:32 +0200

    removed autoscript from fortigate

--- time.tf
+++ time.tf
@@ -1,36 +1,36 @@
 
-locals {
-  now = timestamp()
+# locals {
+#   now = timestamp()
 
-  OSLO_tz = timeadd(local.now, "2h") # OSLO official time (+ 0200 from UTC..?? ))
+#   OSLO_tz = timeadd(local.now, "2h") # OSLO official time (+ 0200 from UTC..?? ))
 
-  date_oslo      = formatdate("YYYY-MM-DD hh:mm:ss", local.OSLO_tz)
-  fortigate_time = formatdate("hh:mm:ss", local.OSLO_tz)
+#   date_oslo      = formatdate("YYYY-MM-DD hh:mm:ss", local.OSLO_tz)
+#   fortigate_time = formatdate("hh:mm:ss", local.OSLO_tz)
 
-  date_utc = formatdate("YYYY-MM-DD", local.now)
-}
-
-
-
-output "test" {
-
-  value = local.fortigate_time
-}
+#   date_utc = formatdate("YYYY-MM-DD", local.now)
+# }
 
 
 
-resource "fortios_system_autoscript" "DateAndTime" {
-  interval    = 120
-  name        = "SET DATE AND TIME -"
-  output_size = 1024
-  repeat      = 0
+# output "test" {
 
-  script = <<EOF
+#   value = local.fortigate_time
+# }
 
 
-#execute date "${formatdate("YYYY-MM-DD", "${timestamp()}")}"
-#execute time "${local.fortigate_time}"
 
-EOF
-  start  = "auto"
-}
+# resource "fortios_system_autoscript" "DateAndTime" {
+#   interval    = 120
+#   name        = "SET DATE AND TIME -"
+#   output_size = 1024
+#   repeat      = 0
+
+#   script = <<EOF
+
+
+# #execute date "${formatdate("YYYY-MM-DD", "${timestamp()}")}"
+# #execute time "${local.fortigate_time}"
+
+# EOF
+#   start  = "auto"
+# }
